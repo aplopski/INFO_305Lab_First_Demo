a650f1492b1d72941f900c2dcc7af1d78221ca38
Alexander Plopski <plopski@is.naist.jp>
2021-03-11 12:40:12 +1300

    Demo App for the first INFO305 Lab

---
+++ Assets/BasicDemo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicDemo : MonoBehaviour
+{
+
+    bool moving = false;
+    float offset = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0, 20 * Time.deltaTime, 0, Space.Self);
+        if (moving)
+        {
+            this.transform.Translate(offset * Time.deltaTime, 0, 0, Space.World);
+            if (Mathf.Abs(this.transform.position.x) > 1)
+            {
+                offset = -offset;
+            }
+
+        }
+        
+    }
+
+    public void toggleMove()
+    {
+        moving = !moving;
+    }
+}
